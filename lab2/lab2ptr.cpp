33ed4ab7af0a9fa7bd99af6554b4c3c104aa5f49
Kou-ding <fotispapadak@gmail.com>
2025-12-17 04:15:21 +0200

    lab2 filter and clipping first try

---
+++ lab2/lab2ptr.cpp
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HEIGHT 256
+#define WIDTH 256
+#define DATA_SIZE HEIGHT*WIDTH
+#define BUFFER_SIZE 256
+#define T1 32
+#define T2 96
+
+int clipper(int element){
+  if (element>255){
+    element=255;
+  }
+  else if (element<0){
+    element=0;
+  }
+  return element;
+}
+
+void imageDiffPosterize(const unsigned int *A,
+                        const unsigned int *B,
+                        unsigned int *C,
+                        int size) {
+
+   // Local buffers that we want to implement in BRAM
+   unsigned int A_buffer[BUFFER_SIZE];
+   unsigned int B_buffer[BUFFER_SIZE];
+   unsigned int C_buffer[BUFFER_SIZE];
+   unsigned int C_filt[BUFFER_SIZE];
+   unsigned int S[(WIDTH-2)*(HEIGHT-2)]={0};
+
+    // Filter
+    int F[3][3] = {0, -1, 0, -1, 5, -1, 0, -1, 0};
+
+    unsigned long int D = 0;
+
+   for (int i = 0; i < size; i += BUFFER_SIZE) {
+// #pragma HLS LOOP_TRIPCOUNT min = c_len max = c_len
+    int chunk_size = BUFFER_SIZE;
+    // boundary checks
+    if ((i + BUFFER_SIZE) > size)
+      chunk_size = size - i;
+
+
+    read1:
+        for (int j = 0; j < chunk_size; j++) {
+    // #pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+    // #pragma HLS PIPELINE II = 1
+        A_buffer[j] = A[i + j];
+        }
+
+    read2:
+        for (int j = 0; j < chunk_size; j++) {
+    // #pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+    // #pragma HLS PIPELINE II = 1
+        B_buffer[j] = B[i + j];
+        }
+
+    vadd:
+        for (int j = 0; j < chunk_size; j++) {
+    // #pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+    // #pragma HLS PIPELINE II = 1
+        // perform vector addition
+        D = A_buffer[j] - B_buffer[j];
+        if(D<0) D=-D;
+        if (D < T1) {
+               C_buffer[j] = 0;
+           } else if (D < T2) {
+               C_buffer[j] = 128;
+           } else {
+               C_buffer[j] = 255;
+           }
+        }
+    // filter:
+    //     for (int k = 0; k < chunk_size; j++) {
+    //         for (int i = 1; i <= HEIGHT - 2; i++) {
+    //             for (int j = 1; j <= WIDTH - 2; j++) {
+    //                 for (int a = -1; a <= 1; a++) {
+    //                     for (int b = -1; b <= 1; b++) {
+    //                         S[(i - 1)*WIDTH+(j - 1)] = S[(i - 1)*WIDTH+(j - 1)] + F[a + 1][b + 1] * C[(i + a)*WIDTH+(j + b)];
+    //                     }
+    //                 }
+    //             }
+    //         }  
+    //     }
+    // // Clipping
+    // for (int i = 0; i < HEIGHT; i++) {
+    //     for (int j = 0; j < WIDTH; j++) {
+    //     if(i>=1 && i<=HEIGHT-2 && j>=1 && j<=WIDTH-2){
+    //         C_filt[i][j]=clipper(S[i-1][j-1]);
+    //     }else{
+    //         C_filt[i][j]=clipper(C[i][j]);
+    //     }
+    //     }
+    // }
+    // burst write the result
+    write:
+        for (int j = 0; j < chunk_size; j++) {
+    // #pragma HLS LOOP_TRIPCOUNT min = c_size max = c_size
+    // #pragma HLS PIPELINE II = 1  
+            C[i + j] = C_buffer[j];
+        }
+    }
+}
+
+void sw_ref(unsigned int *A, unsigned int *B, unsigned int *C_SW){
+    unsigned long int D;
+    for(int i=0;i<HEIGHT;i++){
+        for(int j=0;j<WIDTH;j++){
+            D = A[i*WIDTH + j] - B[i*WIDTH + j];
+            if(D<0) D=-D;
+            if(D<T1){
+                C_SW[i*WIDTH + j]=0;
+            } else if(D<T2){
+                C_SW[i*WIDTH + j]=128;
+            } else {
+                C_SW[i*WIDTH + j]=255;
+            }
+        }
+    }
+}
+
+int main(){
+    // Initialize variables
+    int errors=0;
+    unsigned int *A = (unsigned int*)malloc(DATA_SIZE * sizeof(unsigned int));
+    unsigned int *B = (unsigned int*)malloc(DATA_SIZE * sizeof(unsigned int));
+    unsigned int *C = (unsigned int*)malloc(DATA_SIZE * sizeof(unsigned int));
+    unsigned int *C_SW = (unsigned int*)malloc(DATA_SIZE * sizeof(unsigned int));
+    int size=DATA_SIZE;
+
+    // Populate A and B
+    for(int i=0;i<HEIGHT;i++){
+        for(int j=0;j<WIDTH;j++){
+            A[i*WIDTH+j]=(unsigned int)i;
+            B[i*WIDTH+j]=(unsigned int)j;
+        }
+    }
+
+    // Run functions
+    imageDiffPosterize(A, B, C, size);
+    sw_ref(A, B, C_SW);
+
+    // Calculate SW and HW differences
+    for(int i=0;i<HEIGHT;i++){
+        for(int j=0;j<WIDTH;j++){
+            if(C_SW[i*WIDTH+j]!=C[i*WIDTH+j]){
+                errors++;
+            }
+        }
+    }
+
+    // Report
+    if(errors>=1){
+        printf("There are %d different pixels.\n", errors);
+    }
+    else
+    {
+        printf("Test passed!");
+    }
+    return 0;
+}
+// }
+//    // Copy data from off-chip into BRAM locals
+//    for (int i = 0; i < HEIGHT; ++i) {
+// #pragma HLS pipeline II=1
+//        for (int j = 0; j < WIDTH; ++j) {
+//            A_local[i][j] = A[i][j];
+//            B_local[i][j] = B[i][j];
+//        }
+//    }
+
+//    // Compute using BRAM arrays
+//    for (int i = 0; i < HEIGHT; ++i) {
+
+// // Pipeline the loop
+// #pragma HLS pipeline II=1
+
+//        for (int j = 0; j < WIDTH; ++j) {
+//            int16_t D = (int16_t)A_local[i][j] - (int16_t)B_local[i][j];
+//            if (D < 0) D = -D;
+//            if (D < T1) {
+//                C_local[i][j] = 0;
+//            } else if (D < T2) {
+//                C_local[i][j] = 128;
+//            } else {
+//                C_local[i][j] = 255;
+//            }
+//        }
+//    }
+
+//    // Write back to off-chip
+//    for (int i = 0; i < HEIGHT; ++i) {
+// #pragma HLS pipeline II=1
+//        for (int j = 0; j < WIDTH; ++j) {
+//            C[i][j] = C_local[i][j];
+//        }
+//    }
+// }
